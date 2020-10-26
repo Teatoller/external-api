04638bcb1dfb06bf7b303b366ad4ebb391983d80
Teatoller <steven.otieno1@gmail.com>
2020-10-26 17:41:35 +0300

    (web route): Implement web route
    - return api on web route
    
    [Starts #003]

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use GuzzleHttp\Client;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,13 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+Route::get('/json-api', function() {
+	$client = new Client();
+
+	$response = $client->request('GET', 'https://jsonplaceholder.typicode.com/posts');
+	$statusCode = $response->getStatusCode();
+	$body = $response->getBody()->getContents();
+
+	return $body;
+});
